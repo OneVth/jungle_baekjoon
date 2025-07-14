5416a80f19bf2c3aa5a0ec8f64d0a5f522b789ab
OneVth <onebrotravel.gmail.com>
2025-07-14 21:34:52 +0900

    feat: 2468번 - 안전 영역 문제 BFS 방식 해결

--- 2468/main.py
+++ 2468/main.py
@@ -1,6 +1,5 @@
 import sys
-
-sys.setrecursionlimit(10**8)
+from collections import deque
 
 n = int(sys.stdin.readline())
 
@@ -10,13 +9,19 @@ max_height = max(map(max, table))
 result = 0
 
 
-def dfs(x, y, h):
+def bfs(x, y, h):
+    q = deque()
+    q.append((x, y))
     visited[x][y] = True
-    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < n and 0 <= ny < n:
-            if not visited[nx][ny] and table[nx][ny] > h:
-                dfs(nx, ny, h)
+
+    while q:
+        cx, cy = q.popleft()
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx, ny = cx + dx, cy + dy
+            if 0 <= nx < n and 0 <= ny < n:
+                if not visited[nx][ny] and table[nx][ny] > h:
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
 
 
 for h in range(max_height + 1):
@@ -25,7 +30,7 @@ for h in range(max_height + 1):
     for i in range(n):
         for j in range(n):
             if not visited[i][j] and table[i][j] > h:
-                dfs(i, j, h)
+                bfs(i, j, h)
                 cnt += 1
     result = max(result, cnt)
 
