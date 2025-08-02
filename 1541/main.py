9440a7be31bee26c5c5de8a7b14a4dd970d8356d
OneVth <onebrotravel@gmail.com>
2025-08-02 17:51:09 +0900

    feat: 1541번 - 잃어버린 괄호 문제 해결

---
+++ 1541/main.py
@@ -0,0 +1,9 @@
+import sys
+
+input = sys.stdin.readline
+
+parts = input().split('-')
+result = sum(map(int, parts[0].split('+')))
+for part in parts[1:]:
+    result -= sum(map(int, part.split('+')))
+print(result)
